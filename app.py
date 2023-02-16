cd078d863f64213fe8d67c9961a359070831b06f
Richard Kämmerer <richard.kaemmerer@gmail.com>
2023-02-16 16:15:06 +0100

    test

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 # simple flask app
 from flask import Flask
+import json
+
 app = Flask(__name__)
+
+images = json.load(open('images.json'))
+
+dirs = []
+for image in images:    
+    if image['path'] not in dirs:
+        dirs.append(image['path'])
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    
+    for dir in dirs:
+        print('<a href="/{dir}">{dir}</a>').format(dir=dir)
 
 if __name__ == '__main__':
     app.run()
