a2a12c1075843ab690a01f74fe8660b13bab7817
Richard Kämmerer <richard.kaemmerer@gmail.com>
2023-02-16 19:20:18 +0100

    Functionality, data update

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 # simple flask app
-from flask import Flask
+from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
 
 images = json.load(open('images.json'))
 
-dirs = []
-for image in images:    
-    if image['path'] not in dirs:
-        dirs.append(image['path'])
 
 @app.route('/')
-def hello_world():
-    
-    for dir in dirs:
-        print('<a href="/{dir}">{dir}</a>').format(dir=dir)
+def show_dirs():
+    dirs = []
+
+    for image in images:
+        if image['path'] not in dirs:
+            dirs.append(image['path'])    
+
+    return render_template('index.html', dirs=dirs)
+
+@app.route("/<string:dir>")
+def show_dir(dir):
+    images_in_dir = []
+
+    for image in images:
+        if image['path'] == dir:
+            images_in_dir.append(image)
+
+    return render_template('dir.html', images=images_in_dir, dir=dir)
 
 if __name__ == '__main__':
     app.run()
-
-
